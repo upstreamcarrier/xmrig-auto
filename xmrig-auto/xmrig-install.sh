fa02b3678d252d751e4dcf0f4d7886a27348c7c3
upstreamcarrier <info@upstreamcarrier.com>
2025-08-06 09:11:25 +0530

    Update xmrig-install.sh

--- xmrig-auto/xmrig-install.sh
+++ xmrig-auto/xmrig-install.sh
@@ -5,10 +5,13 @@ echo "Installing dependencies..."
 sudo dnf groupinstall "Development Tools" -y
 sudo dnf install epel-release -y
 sudo dnf install cmake git libmicrohttpd-devel hwloc-devel openssl-devel -y
+sudo dnf install libstdc++-static
 
 echo "Cloning xmrig..."
+mkdir /etc/projects
+echo "created directory /etc/projects"
 git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+cd /etc/projects/xmrig
 mkdir build && cd build
 cmake ..
 make -j$(nproc)
